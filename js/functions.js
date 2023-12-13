898bb6dc8fc9dfe6226947a369e6859f1522bddb
Mister-Axolotl <krlekraft@gmail.com>
2023-12-13 23:06:23 +0100

    litles advancements

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-let lineNumberMessage, arrayContent;
+let lineNumberMessage, lineContent, arrayContent;
 
 var selectedLanguage = "fr";
 
@@ -7,6 +7,7 @@ fetch('./languages/' + selectedLanguage + '.json')
     .then(data => {
         lineNumberMessage = data.lineNumberMessage;
         arrayContent = data.arrayContent;
+        lineContent = data.lineContent;
     })
     .catch(error => console.error('Error loading JSON file:', error));
 
@@ -47,34 +48,69 @@ export function sendArray(content) {
     let lineNumber = 0;
 
     const regex = /"([^"]*)"/g; // Regular expression to extract content enclosed in double quotes
-
+    
     lines.forEach(function (line) {
         lineNumber++;
+        var container = document.getElementById('container');
+        var arrayDiv;
+        if (line.includes('{')) {
+            const matches = line.match(regex);
 
-        if (!line.includes('{') && !line.includes('}')) {
-            if (line.includes('[')) { // detect an array
-                var matches = line.match(regex);
-                var container = document.getElementById('container');
-                var arrayDiv;
+            if (matches) {
+                matches.forEach(match => {
+                    const values = match.substring(1, match.length - 1).split(',').map(value => value.trim());
+                    console.log("Array:", values);
+                });
+            } else {
+                
+                /*matches.forEach(match => {
+                    let array = match.replaceAll('"', '').replaceAll("'", '')
 
-                if (matches) {
-                    matches.forEach(function (match) {
-                        let array = match.replaceAll('"', '').replaceAll("'", '')
+                    arrayDiv = document.createElement('div');
+                    arrayDiv.classList.add('array');
 
-                        arrayDiv = document.createElement('div');
-                        arrayDiv.classList.add('array');
+                    var lineNumberDiv = document.createElement('div');
+                    lineNumberDiv.textContent = lineNumberMessage + lineNumber;
+                    arrayDiv.appendChild(lineNumberDiv);
 
-                        var lineNumberDiv = document.createElement('div');
-                        lineNumberDiv.textContent = lineNumberMessage + lineNumber;
-                        arrayDiv.appendChild(lineNumberDiv);
-
-                        var arrayContentDiv = document.createElement('div');
-                        arrayContentDiv.textContent = arrayContent + array;
-                        arrayDiv.appendChild(arrayContentDiv);
-                    });
-                }
-                container.appendChild(arrayDiv);
+                    var arrayContentDiv = document.createElement('div');
+                    arrayContentDiv.textContent = arrayContent + array;
+                    arrayDiv.appendChild(arrayContentDiv);
+                });*/
             }
+        } else {
+            var arrayContentDiv = document.createElement('div');
+            arrayDiv = document.createElement('div');
+            arrayDiv.classList.add('array');
+            let array = line.replaceAll('"', '').replaceAll("'", '');
+            arrayContentDiv.textContent = lineContent + array;
+            arrayDiv.appendChild(arrayContentDiv);
+            container.appendChild(arrayDiv);
         }
     });
 }
+
+/*if (!line.includes('{') && !line.includes('}')) {
+    if (line.includes('[')) { // detect an array
+        var matches = line.match(regex);
+        var container = document.getElementById('container');
+        var arrayDiv;
+        if (matches) {
+            matches.forEach(function (match) {
+                let array = match.replaceAll('"', '').replaceAll("'", '')
+
+                arrayDiv = document.createElement('div');
+                arrayDiv.classList.add('array');
+
+                var lineNumberDiv = document.createElement('div');
+                lineNumberDiv.textContent = lineNumberMessage + lineNumber;
+                arrayDiv.appendChild(lineNumberDiv);
+
+                var arrayContentDiv = document.createElement('div');
+                arrayContentDiv.textContent = arrayContent + array;
+                arrayDiv.appendChild(arrayContentDiv);
+            });
+        }
+        container.appendChild(arrayDiv);
+    }
+}*/
